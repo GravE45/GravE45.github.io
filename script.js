d6ffc1e410e1b5f11e3dcce455d65c3d5bf5d040
GravE45 <stevejacobt@gmail.com>
2024-05-02 21:07:06 +0900

    chanages

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ function toggleMenu() {
     // Open the default email client with the pre-filled email
     window.location.href = mailtoURL;
   }
+  function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
+
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
+
+// Close modal on outside click
+window.onclick = function(event) {
+    var modals = document.querySelectorAll('.modal');
+    modals.forEach(function(modal) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+}
